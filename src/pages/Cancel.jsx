8412d822a27d35949123cddfb9f0e334cc6ae438
FedorPortnoi <195462502+FedorPortnoi@users.noreply.github.com>
2026-04-22 12:53:30 -0500

    feat: send SMS to Jen when booking is cancelled

--- src/pages/Cancel.jsx
+++ src/pages/Cancel.jsx
@@ -3,6 +3,7 @@ import { useSearchParams, Link } from 'react-router-dom'
 import Header from '../components/Header.jsx'
 import { cancelBooking } from '../utils/bookings.js'
 import { isSlotCancellable, formatDateDisplay } from '../utils/slots.js'
+import { sendCancellationNotification } from '../utils/email.js'
 
 export default function Cancel() {
   const [params] = useSearchParams()
@@ -20,6 +21,7 @@ export default function Cancel() {
     setStatus('loading')
     try {
       await cancelBooking({ bookingId })
+      sendCancellationNotification({ name: 'A client', date: dateDisplay, time }).catch(() => {})
       setStatus('success')
     } catch {
       setStatus('error')
